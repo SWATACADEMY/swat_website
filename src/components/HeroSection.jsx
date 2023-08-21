ed94610af6e16fa07e4d40a1b5cd9de5a812985c
Jolextom <goldenprince724@gmail.com>
2023-08-21 09:03:22 +0100

    partners logo animation (DONE)

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,9 +1,23 @@
+import { useState } from "react";
 import { slideshowsPartner } from "../utils/slideshow";
 import Navbar from "./Navbar";
 import { PrimaryButton, SecondaryButton } from "./common/Button";
 import ArrowDown from "./svgs/ArrowDown";
 
 const HeroSection = () => {
+  const [active, setActive] = useState(false);
+
+  const handleEnter = (image) => {
+    image.style.filter = "grayscale(0%)";
+    image.style.transform = "scale(1.5)";
+    setActive(true);
+  };
+  const handleLeave = (image) => {
+    image.style.filter = "grayscale(100%)";
+    image.style.transform = "scale(1)";
+    setActive(false);
+  };
+
   return (
     <div className="flex flex-col space-y-8  mb-20 lg:pb-[30px] w-screen  h-screen">
       <div className="flex  bg-hero_bg flex-col bg-black min-h-[100vh]  bg-no-repeat bg-cover bg-center items-center justify-between w-full ">
@@ -37,9 +51,19 @@ const HeroSection = () => {
       {/* arrow */}
 
       <div className=" w-full max-w-[100%] z-[9999] gap-x-10  flex">
-        <div className="flex animate-slides_linear  items-center gap-x-10 ">
-          {slideshowsPartner.map((data) => (
-            <img className=" images block w-12 h-12  " key={data} src={data} />
+        <div
+          className={`flex animate-slides_linear  items-center gap-x-10 ${
+            active ? " paused " : ""
+          }`}
+        >
+          {slideshowsPartner.map((data, index) => (
+            <img
+              className={` images block w-12 h-12`}
+              onMouseEnter={(e) => handleEnter(e.target)}
+              onMouseLeave={(e) => handleLeave(e.target)}
+              key={index}
+              src={data}
+            />
           ))}
         </div>
       </div>
